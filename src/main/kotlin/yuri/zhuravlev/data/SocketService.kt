5083475e76e80e660ce104a60b9c273074160f41
zhuravlevyuri <zhuravlevyuri2000@gmail.com>
2022-04-17 22:27:39 +0300

    socket init

--- src/main/kotlin/yuri/zhuravlev/data/SocketService.kt
+++ src/main/kotlin/yuri/zhuravlev/data/SocketService.kt
@@ -5,5 +5,7 @@ import com.google.gson.Gson
 object SocketService {
     val gson = Gson()
 
-
+    fun proceed(text: String, onSend: suspend (String) -> Unit, onClose: suspend (String) -> Unit) {
+        // TODO
+    }
 }
--- src/main/kotlin/yuri/zhuravlev/plugins/Sockets.kt
+++ src/main/kotlin/yuri/zhuravlev/plugins/Sockets.kt
@@ -5,7 +5,10 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
+import kotlinx.coroutines.channels.ClosedReceiveChannelException
+import yuri.zhuravlev.data.SocketService
 import java.time.Duration
+import java.util.*
 
 fun Application.configureSockets() {
     install(WebSockets) {
@@ -16,17 +19,36 @@ fun Application.configureSockets() {
     }
 
     routing {
+        val socketService = SocketService
+        val connections = Collections.synchronizedSortedMap<WebSocketServerSession, String>(TreeMap())
+
         webSocket("/") { // websocketSession
-            for (frame in incoming) {
-                when (frame) {
-                    is Frame.Text -> {
-                        val text = frame.readText()
-                        outgoing.send(Frame.Text("YOU SAID: $text"))
-                        if (text.equals("bye", ignoreCase = true)) {
-                            close(CloseReason(CloseReason.Codes.NORMAL, "Client said BYE"))
+            println("onConnect")
+            try {
+                for (frame in incoming) {
+                    when (frame) {
+                        is Frame.Text -> {
+                            if (this !in connections.keys) {
+                                // create connection
+                                val name = frame.readText()
+                                connections += this to name
+                                send("Login as [$name]")
+                            } else {
+                                socketService.proceed(text = frame.readText(), onSend = { text ->
+                                    outgoing.send(Frame.Text("YOU SAID: $text"))
+                                }, onClose = {
+                                    close(CloseReason(CloseReason.Codes.NORMAL, "Client said BYE"))
+                                }
+                                )
+                            }
                         }
                     }
                 }
+            } catch (e: ClosedReceiveChannelException) {
+                println("onClose ${closeReason.await()}")
+            } catch (e: Throwable) {
+                println("onError ${closeReason.await()}")
+                e.printStackTrace()
             }
         }
 
